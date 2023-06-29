6fea6229a651b56ded3c0745f23a589eed523357
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-06-29 07:02:42 -0500

    feat (Backend) : ✨ GetID,Put,Delete Categorias
    Creacion del EndPoint para el GetID y solo recibir un elemento el Put para modificar un element y el Delete para eliminar un elemento

--- Backend/src/controllers/categoria.controllers.js
+++ Backend/src/controllers/categoria.controllers.js
@@ -3,7 +3,7 @@ import getConnection from "./../db/database.js";
 export const getCategorias = async (req, res) => {
   try {
     const connection = await getConnection();
-    const sql = "SELECT * FROM categorias";
+    const sql = `SELECT * FROM categorias`;
     const result = await connection.query(sql);
     console.log(result);
     res.json(result);
@@ -13,7 +13,21 @@ export const getCategorias = async (req, res) => {
   }
 };
 
-export const postCategorias = async (req, res) => {
+export const getCategoria = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const { id } = req.params;
+    const sql = `SELECT * FROM categorias WHERE CategoriaID = ?`;
+    const result = await connection.query(sql,id);
+    console.log(result);
+    res.json(result);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+export const postCategoria = async (req, res) => {
   try {
     console.log(req.body);
     const { CategoriaNombre, Descripcion, Imagen } = req.body;
@@ -25,6 +39,41 @@ export const postCategorias = async (req, res) => {
       Descripcion,
       Imagen,
     ]);
+    console.log(result);
+    res.json(result);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+export const putCategoria = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { CategoriaNombre, Descripcion, Imagen } = req.body;
+
+    const connection = await getConnection();
+    const sql = `UPDATE categorias SET CategoriaNombre =  ?, Descripcion = ?, Imagen = ? WHERE CategoriaID = ?`;
+    const result = await connection.query(sql, [
+      CategoriaNombre,
+      Descripcion,
+      Imagen,
+    ],id);
+    console.log(result);
+    res.json(result);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+export const deleteCategorias = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const connection = await getConnection();
+    const sql = `DELETE FROM categorias WHERE CategoriaID = ?`;
+    const result = await connection.query(sql,id);
     console.log(result);
     res.json(result);
   } catch (error) {
--- Backend/src/routes/categorias.routes.js
+++ Backend/src/routes/categorias.routes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import {
   getCategorias,
-  postCategorias,
+  getCategoria,
+  postCategoria,
+  putCategoria,
+  deleteCategorias,
 } from "../controllers/categoria.controllers.js";
 
 const router = Router();
 
 router.get("/api/categorias", getCategorias);
-router.post("/api/categorias", postCategorias);
+router.get("/api/categorias/:id", getCategoria);
+router.post("/api/categorias", postCategoria);
+router.put("/api/categorias/:id", putCategoria);
+router.delete("/api/categorias/:id", deleteCategorias);
 
 export default router;
